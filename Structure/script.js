465274520fa8bb4f1be1cd8cc617fa0ce7f2cc0b
Tarun Swami <tarun2002swami@gmail.com>
2023-04-25 17:20:26 +0530

    update

---
+++ Structure/script.js
@@ -0,0 +1,44 @@
+const observer1 = new IntersectionObserver ((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenRepeat = document.querySelectorAll('.hidden_rep');
+hiddenRepeat.forEach((el) => observer1.observe(el));
+
+const observer2 = new IntersectionObserver ((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add ('show');
+    } 
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer2.observe(el));
+
+// follower //
+
+let area = document.querySelector('body').getBoundingClientRect();
+let follower = document.getElementById('follower');
+
+document.addEventListener('mousemove', function(e) {
+  let left = e.clientX;
+  let top = e.clientY;
+  let insideArea = (left >= area.left && left <= area.right && top >= area.top && top <= area.bottom);
+
+  if (insideArea) {
+    follower.style.opacity = 1;
+    follower.style.left = left + 'px';
+    follower.style.top = top + 'px';
+  } else {
+    follower.style.opacity = 0;
+  }
+});
