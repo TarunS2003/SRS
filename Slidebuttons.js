6f1a1d83231d4d6ffe2e77ba2f0b2f07cbe114fe
TarunS2003 <tarun2002swami@gmail.com>
2023-07-02 08:36:22 +0530

    'update'

---
+++ Slidebuttons.js
@@ -0,0 +1,62 @@
+//---- slide buttons ---- //
+const forwardButton = document.querySelector('.forwardbutton');
+const backwardButton = document.querySelector('.prevbutton');
+const slideContainers = document.querySelectorAll('.slide');
+
+// Add event listeners to the buttons
+forwardButton.addEventListener('click', handleForwardButtonClick);
+backwardButton.addEventListener('click', handleBackwardButtonClick);
+
+// Event handler for the forward button click
+function handleForwardButtonClick() {
+  const activeIndex = getActiveIndex();
+  const nextIndex = getNextIndex(activeIndex);
+
+  if (nextIndex !== -1) {
+    slideContainers[activeIndex].classList.remove('active');
+    slideContainers[activeIndex].classList.add('prev');
+
+    slideContainers[nextIndex].classList.remove('next');
+    slideContainers[nextIndex].classList.add('active');
+  }
+}
+
+// Event handler for the backward button click
+function handleBackwardButtonClick() {
+  const activeIndex = getActiveIndex();
+  const prevIndex = getPrevIndex(activeIndex);
+
+  if (prevIndex !== -1) {
+    slideContainers[activeIndex].classList.remove('active');
+    slideContainers[activeIndex].classList.add('next');
+
+    slideContainers[prevIndex].classList.remove('prev');
+    slideContainers[prevIndex].classList.add('active');
+  }
+}
+
+// Helper function to get the index of the active slide container
+function getActiveIndex() {
+  for (let i = 0; i < slideContainers.length; i++) {
+    if (slideContainers[i].classList.contains('active')) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Helper function to get the index of the next slide container
+function getNextIndex(activeIndex) {
+  if (activeIndex !== -1 && activeIndex < slideContainers.length - 1) {
+    return activeIndex + 1;
+  }
+  return -1;
+}
+
+// Helper function to get the index of the previous slide container
+function getPrevIndex(activeIndex) {
+  if (activeIndex !== -1 && activeIndex > 0) {
+    return activeIndex - 1;
+  }
+  return -1;
+}
--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-//---- repeat hiding ---- //
-const observer1 = new IntersectionObserver ((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
-
-const hiddenRepeat = document.querySelectorAll('.hidden_rep');
-hiddenRepeat.forEach((el) => observer1.observe(el));
-
-//---- hidden for once ----//
-const observer2 = new IntersectionObserver ((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add ('show');
-    } 
-  });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer2.observe(el));
-
 // -----slide from top to bottom ---- //
 const observer3 = new IntersectionObserver ((entries) => {
   entries.forEach((entry) => {
@@ -178,8 +150,8 @@ const backwardButton = document.querySelector('.prevbutton');
 const slideContainers = document.querySelectorAll('.slide');
 
 // Add event listeners to the buttons
-forwardButton.addEventListener('click', handleForwardButtonClick);
-backwardButton.addEventListener('click', handleBackwardButtonClick);
+// forwardButton.addEventListener('click', handleForwardButtonClick);
+// backwardButton.addEventListener('click', handleBackwardButtonClick);
 
 // Event handler for the forward button click
 function handleForwardButtonClick() {
@@ -235,8 +207,30 @@ function getPrevIndex(activeIndex) {
   return -1;
 }
 
+// sidebar
+let sidebar = document.querySelector('.sidebar');
 
+function hidenav() {
+  sidebar.classList.add('hidenav');
+}
 
-// hide sidebar //
+function shownav() {
+  sidebar.classList.remove('hidenav');
+}
+
+const observer1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      hidenav();
+    } else {
+      shownav();
+    }
+  });
+}, {
+   threshold: 0.1
+});
+
+const navbar = document.querySelector('header');
+observer1.observe(navbar);
 
 
